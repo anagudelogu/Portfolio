bf2239b9a1af03221243eba54b4d91e8495f8dc6
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-04 23:19:26 +0100

    Add the function to the dom

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let hamburguer = document.querySelector('.nav__icon');
-let open = document.querySelector('.nav__hamburguer');
-let close = document.querySelector('.nav__close');
-let menu = document.querySelector('.nav__links');
-let navElement = document.querySelectorAll('.nav__link');
+let hamburger = document.querySelector('.nav__icon')
+let open = document.querySelector('.nav__hamburguer')
+let close = document.querySelector('.nav__close')
+let menu = document.querySelector('.nav__links')
+let navElement = document.querySelectorAll('.nav__link')
+let showMenu = document.querySelector(".showMenu");
 
-
-function toggle(){
-    if (menu.classList.contains('showMenu')){
-        menu.classList.remove('showMenu')
-        close.style.display='none';
-        open.style.display='block';
-    } else{
-        menu.classList.add('showMenu');
-        close.style.display='block';
-        open.style.display='none';
-    }
+function toggle() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu')
+    showMenu.style.display = 'none'
+    close.style.display = 'none'
+    open.style.display = 'block'
+  } else {
+    menu.classList.add('showMenu')
+    close.style.display = 'block'
+    showMenu.style.display = 'flex'
+    open.style.display = 'none'
+  }
 }
+
+hamburger.addEventListener('click', toggle)
