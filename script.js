2c02eabff813b616eb8c077c5ac9ddd7737540e6
Andres Agudelo <anagudelogu@gmail.com>
2022-04-04 20:45:09 -0500

    Fix linter errors

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-let hamburger = document.querySelector('.nav__icon')
-let open = document.querySelector('.nav__hamburguer')
-let close = document.querySelector('.nav__close')
-let menu = document.querySelector('.nav__links')
-let navElement = document.querySelectorAll('.nav__link')
-let showMenu = document.querySelector(".showMenu");
+const hamburger = document.querySelector('.nav__icon');
+const open = document.querySelector('.nav__hamburguer');
+const close = document.querySelector('.nav__close');
+const menu = document.querySelector('.nav__links');
+const showMenu = document.querySelector('.showMenu');
 
 function toggle() {
   if (menu.classList.contains('showMenu')) {
-    menu.classList.remove('showMenu')
-    showMenu.style.display = 'none'
-    close.style.display = 'none'
-    open.style.display = 'block'
+    menu.classList.remove('showMenu');
+    showMenu.style.display = 'none';
+    close.style.display = 'none';
+    open.style.display = 'block';
   } else {
-    menu.classList.add('showMenu')
-    close.style.display = 'block'
-    showMenu.style.display = 'flex'
-    open.style.display = 'none'
+    menu.classList.add('showMenu');
+    close.style.display = 'block';
+    showMenu.style.display = 'flex';
+    open.style.display = 'none';
   }
 }
 
-hamburger.addEventListener('click', toggle)
+hamburger.addEventListener('click', toggle);
