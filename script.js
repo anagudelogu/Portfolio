15f8bf039e2a2f9c5df85725598e2c5145aa7cb1
Andres Agudelo <anagudelogu@gmail.com>
2022-04-05 11:29:12 -0500

    Clean code
    1. Correct a typo on a CSS selector.
    2. Run HTML, CSS and Javascript linters.
    3. Delete active variable, unused on javascript.

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
 const hamburger = document.querySelector('.nav__icon');
-// const open = document.querySelector('.nav__hamburguer');
-// const close = document.querySelector('.nav__close');
 const navMenu = document.querySelector('.nav__links');
-const active = document.querySelector('.active');
-const body = document.body;
+const { body } = document;
 const links = document.querySelectorAll('.nav__link');
 
 function toggle() {
-//   if (menu.classList.contains('showMenu')) {
-//     menu.classList.remove('showMenu');
-//     // menu.style.display = 'none';
-//     close.style.display = 'none';
-//     open.style.display = 'block';
-//   } else {
-//     menu.classList.add('showMenu');
-//     close.style.display = 'block';
-//     // menu.style.display = 'flex';
-//     open.style.display = 'none';
-//   }
-
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-    body.classList.toggle('noScroll');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  body.classList.toggle('noScroll');
 }
 
-function linkClicked(){
+function linkClicked() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
   body.classList.remove('noScroll');
 }
 
 hamburger.addEventListener('click', toggle);
-links.forEach(n => n.addEventListener('click', linkClicked));
+links.forEach((n) => n.addEventListener('click', linkClicked));
