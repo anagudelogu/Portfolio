103e4d961aef159c85cad5b0589d0b966f3cc37b
Andres Agudelo <anagudelogu@gmail.com>
2022-04-05 11:23:52 -0500

    Fix logic behind mobile menu script and some design features
    1. Changed the logic of the display menu script.
    2. Fixed some issues with the design, in both, mobile and desktop versions.
    3. Clean up the code.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const hamburger = document.querySelector('.nav__icon');
-const open = document.querySelector('.nav__hamburguer');
-const close = document.querySelector('.nav__close');
-const menu = document.querySelector('.nav__links');
-const showMenu = document.querySelector('.showMenu');
+// const open = document.querySelector('.nav__hamburguer');
+// const close = document.querySelector('.nav__close');
+const navMenu = document.querySelector('.nav__links');
+const active = document.querySelector('.active');
+const body = document.body;
+const links = document.querySelectorAll('.nav__link');
 
 function toggle() {
-  if (menu.classList.contains('showMenu')) {
-    menu.classList.remove('showMenu');
-    showMenu.style.display = 'none';
-    close.style.display = 'none';
-    open.style.display = 'block';
-  } else {
-    menu.classList.add('showMenu');
-    close.style.display = 'block';
-    showMenu.style.display = 'flex';
-    open.style.display = 'none';
-  }
+//   if (menu.classList.contains('showMenu')) {
+//     menu.classList.remove('showMenu');
+//     // menu.style.display = 'none';
+//     close.style.display = 'none';
+//     open.style.display = 'block';
+//   } else {
+//     menu.classList.add('showMenu');
+//     close.style.display = 'block';
+//     // menu.style.display = 'flex';
+//     open.style.display = 'none';
+//   }
+
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    body.classList.toggle('noScroll');
+}
+
+function linkClicked(){
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  body.classList.remove('noScroll');
 }
 
 hamburger.addEventListener('click', toggle);
+links.forEach(n => n.addEventListener('click', linkClicked));
